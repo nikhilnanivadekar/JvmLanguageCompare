b5445d714c5821a3e7bd20a09b65f6e82e99edfe
Nikhil Nanivadekar <nikhilnanivadekar@gmail.com>
2018-09-30 12:54:10 -0600

    Add Kotlin Module.

---
+++ kotlin-module/src/main/kotlin/jvm/language/compare/kotlin/HelloWorld.kt
@@ -0,0 +1,5 @@
+package jvm.language.compare.kotlin
+
+fun main(args: Array<String>) {
+    println("Hello World from Kotlin")
+}
---
+++ kotlin-module/src/test/kotlin/jvm/language/compare/kotlin/CanaryTest.kt
@@ -0,0 +1,17 @@
+package jvm.language.compare.kotlin
+
+import org.junit.Assert
+import org.junit.Test
+
+class CanaryTest {
+
+    @Test
+    fun canaryTest() {
+        Assert.assertFalse("This is a Canary Test", false)
+    }
+
+    @Test
+    fun canaryTestFails() {
+        Assert.assertFalse("This is a Canary Test", true)
+    }
+}
