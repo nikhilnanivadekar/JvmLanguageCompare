24ae02191e252917656db166c4279e7e9c42d4e0
Nikhil Nanivadekar <nikhilnanivadekar@gmail.com>
2018-09-30 11:55:41 -0600

    Add Java Module

---
+++ java-module/src/test/java/jvm/language/compare/java/CanaryTest.java
@@ -0,0 +1,14 @@
+package jvm.language.compare.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CanaryTest
+{
+    @Test
+    public void canary()
+    {
+        Assert.assertFalse("This is a Canary Test-1", false);
+        Assert.assertFalse("This is a Canary Test-2", true);
+    }
+}
