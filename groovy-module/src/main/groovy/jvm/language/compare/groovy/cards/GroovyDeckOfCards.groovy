59dc8ad039866daec5606691923d12bc899e2900
Nikhil Nanivadekar <nikhilnanivadekar@gmail.com>
2018-10-13 14:44:22 -0600

    Make behavior of Groovy Deck of Cards consistent with JDK Deck of Cards

--- groovy-module/src/main/groovy/jvm/language/compare/groovy/cards/GroovyDeckOfCards.groovy
+++ groovy-module/src/main/groovy/jvm/language/compare/groovy/cards/GroovyDeckOfCards.groovy
@@ -3,33 +3,41 @@ package jvm.language.compare.groovy.cards
 class GroovyDeckOfCards {
     private List<Card> cards
     private Map<Suit, List<Card>> cardsBySuit
+    private List<Card> deck = new ArrayList<>()
 
     GroovyDeckOfCards() {
         this.cards = Card.cards().sort()
         this.cardsBySuit = this.cards.groupBy { it.getSuit() }
     }
 
-    List<Card> shuffle(Random random) {
-        List<Card> shuffled = new ArrayList<>(this.cards)
-        3.times { Collections.shuffle(shuffled, random) }
-        shuffled
+    void shuffle(Random random) {
+        def cardCopy = new ArrayList<>(this.cards)
+        3.times { Collections.shuffle(cardCopy, random) }
+        // The list has to be reversed to behave like a Stack similar to other decks
+        // The pop() method in Groovy prior to 2.5 removed elements from the end of the List.
+        // Starting 2.5+ the pop() removes from the front of the list.
+        this.deck = cardCopy.reverse()
     }
 
-    List<Set<Card>> dealHands(List<Card> shuffled, int count, int cardsPerHand) {
-        (1..count).collect({ this.deal(shuffled, cardsPerHand) })
+    Set<Card> deal(int count) {
+        (1..count).collect(new HashSet<>(), { this.deck.pop() }) as Set<Card>
     }
 
-    Set<Card> deal(List<Card> stack, int count) {
-        (1..count).collect(new HashSet<>(), { stack.pop() }) as Set<Card>
+    Card dealOneCard() {
+        return this.deck.pop()
     }
 
-    Card dealOneCard(List<Card> stack) {
-        return stack.pop()
+    int cardsLeftInDeck() {
+        this.deck.size()
     }
 
     List<Set<Card>> shuffleAndDeal(Random random, int hands, int cardsPerHand) {
-        List<Card> shuffled = this.shuffle(random)
-        this.dealHands(shuffled, hands, cardsPerHand)
+        this.shuffle(random)
+        this.dealHands(hands, cardsPerHand)
+    }
+
+    List<Set<Card>> dealHands(int count, int cardsPerHand) {
+        (1..count).collect({ this.deal(cardsPerHand) })
     }
 
     List<Card> diamonds() {
