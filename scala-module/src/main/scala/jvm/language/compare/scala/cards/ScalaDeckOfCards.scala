79d61410444b0d4123c106b0ef90f767dba4c03f
Mark van Buskirk <markvanbuskirk@gmail.com>
2018-11-13 10:45:57 -0500

    Switched back to using mutable collections in ScalaDeckOfCards. Shuffling 3 times.

--- scala-module/src/main/scala/jvm/language/compare/scala/cards/ScalaDeckOfCards.scala
+++ scala-module/src/main/scala/jvm/language/compare/scala/cards/ScalaDeckOfCards.scala
@@ -3,54 +3,58 @@ package jvm.language.compare.scala.cards
 import jvm.language.compare.scala.cards.Rank.Rank
 import jvm.language.compare.scala.cards.Suit.Suit
 
-import scala.collection.immutable.Seq
 import scala.collection.mutable
 import scala.util.Random
 
 class ScalaDeckOfCards() {
-  val cards: List[Card] = Card.getCards.toList.sorted
+  val cards: Seq[Card] = Card.getCards.toBuffer.sorted
   val cardsBySuit: Map[Suit, Seq[Card]] = cards.groupBy(_.suit)
   val deck: mutable.ArrayStack[Card] = mutable.ArrayStack()
 
   def shuffle(random: Random) {
-    val cardsShuffled: List[Card] = random.shuffle(cards)
+    val cardsShuffled: Seq[Card] =
+      random.shuffle(
+        random.shuffle(
+          random.shuffle(cards)
+        )
+      )
     cardsShuffled.foreach((card: Card) => deck.push(card))
-// More advanced but easier to write &read when you get the hang of it.
-//    cardsShuffled.foreach(deck.push)
+    // More advanced but easier to write &read when you get the hang of it.
+    //    cardsShuffled.foreach(deck.push)
   }
 
   def deal(count: Int): mutable.HashSet[Card] = {
     val set: mutable.HashSet[Card] = mutable.HashSet()
-    1.to(count).foreach(_ => set.add(this.deck.pop()))
+    1.to(count).foreach(_ => set.add(deck.pop()))
     set
   }
 
-  def dealOneCard(): Card = this.deck.pop
+  def dealOneCard(): Card = deck.pop
 
-  def cardsLeftInDeck(): Int = this.deck.size
+  def cardsLeftInDeck(): Int = deck.size
 
   def shuffleAndDeal(random: Random, hands: Int, cardsPerHand: Int): Seq[mutable.HashSet[Card]] = {
-    this.shuffle(random)
-    this.dealHands(hands, cardsPerHand)
+    shuffle(random)
+    dealHands(hands, cardsPerHand)
   }
 
   def dealHands(hands: Int, cardsPerHand: Int): Seq[mutable.HashSet[Card]] = {
-    1.to(hands).map(_ => this.deal(cardsPerHand)).toList
+    1.to(hands).map(_ => deal(cardsPerHand)).toList
   }
 
-  def diamonds: Seq[Card] = this.cardsBySuit(Suit.DIAMONDS)
+  def diamonds: Seq[Card] = cardsBySuit(Suit.DIAMONDS)
 
-  def hearts: Seq[Card] = this.cardsBySuit(Suit.HEARTS)
+  def hearts: Seq[Card] = cardsBySuit(Suit.HEARTS)
 
-  def spades: Seq[Card] = this.cardsBySuit(Suit.SPADES)
+  def spades: Seq[Card] = cardsBySuit(Suit.SPADES)
 
-  def clubs: Seq[Card] = this.cardsBySuit(Suit.CLUBS)
+  def clubs: Seq[Card] = cardsBySuit(Suit.CLUBS)
 
-  def countsBySuit: Map[Suit, Int] = this.cards.groupBy(_.suit).mapValues(_.length)
+  def countsBySuit: Map[Suit, Int] = cards.groupBy(_.suit).mapValues(_.length)
 
-  def countsByRank: Map[Rank, Int] = this.cards.groupBy(_.rank).mapValues(_.length)
+  def countsByRank: Map[Rank, Int] = cards.groupBy(_.rank).mapValues(_.length)
 
-  def getCards: Seq[Card] = this.cards
+  def getCards: Seq[Card] = cards
 
-  def getCardsBySuit: Map[Suit, Seq[Card]] = this.cardsBySuit
+  def getCardsBySuit: Map[Suit, Seq[Card]] = cardsBySuit
 }
